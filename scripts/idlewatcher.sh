18751916e52925dbb206c94823312b0e3e23b00b
Lys3rg <lys3rg@debian>
2016-04-24 13:40:00 +0200

    Change structure

--- scripts/idlewatcher.sh
+++ scripts/idlewatcher.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+# Benötigt das Paket xprintidle
 
 USERNAME="infouser"	# Sollte eigentlich immer infouser sein
 MAX_IDLE_TIME=6000	# In Millisekunden
-BROWSER_NAME="chrome"
+BROWSER_NAME="chrome"	# chrome,firefox,iceweasel
+
+LAST_IDLE_TIME=0	
 
 while true; do
   CURRENT_IDLE_TIME=$(export DISPLAY=:0 && sudo -u $USERNAME xprintidle)
