6abe26e67f693fd13a84390f05c95e7b1444282b
Lys3rg <lys3rg@debian>
2016-04-24 18:54:55 +0200

    Install Script

--- scripts/idlewatcher.sh
+++ scripts/idlewatcher.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # Benötigt das Paket xprintidle
 
-USERNAME="infouser"	# Sollte eigentlich immer infouser sein
+USERNAME="lys3rg"	# Sollte eigentlich immer infouser sein
 MAX_IDLE_TIME=6000	# In Millisekunden
 BROWSER_NAME="chrome"	# chrome,firefox,iceweasel
 
-LAST_IDLE_TIME=0	
+LAST_IDLE_TIME=100000	
 
 while true; do
   CURRENT_IDLE_TIME=$(export DISPLAY=:0 && sudo -u $USERNAME xprintidle)
-  if(($MAX_IDLE_TIME < $CURRENT_IDLE_TIME)); then
-    killall $BROWSER_NAME
+  if(($MAX_IDLE_TIME <= $CURRENT_IDLE_TIME)); then
+    if(($LAST_IDLE_TIME >= $CURRENT_IDLE_TIME)); then
+      #killall $BROWSER_NAME
+      echo "Killed Browser $BROWSER_NAME"
+      echo "$LAST_IDLE_TIME"
+      LAST_IDLE_TIME=$CURRENT_IDLE_TIME
+      echo "$LAST_IDLE_TIME"
+    fi
   fi
 done
 
